f4e7d7118a5b8c64325e41ca51f7ea97a150bef3
Victoria Curl <victoriacurl9@gmail.com>
2020-02-10 14:58:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,39 @@
 class Triangle
- attr_accessor :equilateral, :isosceles, :scalene
+  def initialize(side_1, side_2, side_3)
+    @triangle_sides = []
+    @triangle_sides << side_1
+    @triangle_sides << side_2
+    @triangle_sides << side_3
+  end
 
- def initialize(side_a, side_b, side_c)
-@triangle_sides = []
-@triangle_sides << side_a
-@triangle_sides << side_b
-@triangle_sides << side_c
- end
+  def valid?
+    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+
+    if (@triangle_sides.none? {|side| side <= 0}) &&
+      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+      return true
+    else
+      return false
+    end
+  end
+
+  def kind
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+end
+
+class TriangleError < StandardError
+
 end
